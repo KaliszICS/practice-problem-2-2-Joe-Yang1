c3c14d7ab4fc60225ef9b33873f3347bb06fdaca
Joe-Yang1 <348993072@tdsb.ca>
2026-03-11 15:40:09 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,27 @@ public class PracticeProblem {
 
 	}
 
+	public static boolean isEven(int num){
+	return num %2==0;
+	}
+	
+	public static boolean isOdd(int num){
+	return num %2 !=0;
+	}
+
+	public static boolean isPositive(int num){
+	return num > 0;
+	}
+
+	public static boolean isNegative(int num){
+	return num < 0;
+	}
+
+	public static int combinedLength(String word, String word2){
+	String result = word + word;
+	return result.length();
+	
+}
+
+
 }
